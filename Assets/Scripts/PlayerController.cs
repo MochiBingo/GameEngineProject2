3767d18635875f2ddbeed2db208fa17801c798f0
MochiBingo <mochibingoshep@gmail.com>
2025-02-20 14:37:53 -0400

    movement added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameManager gameManager;
+    public Rigidbody2D player;
+    public float moveSpeed = 30f;
+    private Vector2 moveVector = Vector2.zero;
+
+    public void Start()
+    {
+        Actions.moveEvent += updateMoveVector;
+    }
+    public void Update()
+    {
+        moveChar(moveVector);
+    }
+    public void moveChar(Vector2 MoveVector)
+    {
+        player.MovePosition(player.position + moveVector * moveSpeed * Time.deltaTime);
+    }
+    private void updateMoveVector(Vector2 vector)
+    {
+        moveVector = vector;
+    }
 }
